40ded75531b44b50c216782ed8287a07e9776492
Oleksii Cherep <alex1998120850@gmail.com>
2025-10-28 19:39:01 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
+const loginForm = document.querySelector('.login-form')
+loginForm.addEventListener('submit', handleFormSubmit)
 
+function handleFormSubmit(event) {
+    event.preventDefault()
+    const form = event.currentTarget
+    const emailInput = form.elements.email
+    const passwordIput = form.elements.password
+    const emailValue = emailInput.value.trim()
+    const passwordValue = passwordIput.value.trim()
+    if (emailValue === '' || passwordValue === '') {
+        alert('All form fields must be filled in')
+        return
+    }
+    const formData = {
+        email: emailValue,
+        password: passwordValue,
+    }
+    console.log(formData)
+    form.reset()
+}
